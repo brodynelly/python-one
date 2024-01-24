c0cdaecf8c48817d2f5c2a76363ca5f484595214
brodynelly <127979848+brodynelly@users.noreply.github.com>
2024-01-24 17:25:19 -0600

    First project completed
    This is the completement of the first assignment in my python class

---
+++ AboutMe/aboutme.py
@@ -0,0 +1,4 @@
+print("1. Name: Brody Nelson")
+print("2. I was on a study abroad program last semester")
+print("3. this is my 2nd year at university")
+print("4. I am currently living off campus")
